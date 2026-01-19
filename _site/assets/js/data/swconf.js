4ab19a06b08a024bc12a18e841eb4c3aa76c5f73
Ur <gottisttot4678@gmail.com>
2026-01-19 12:10:02 +0900

    Fix: ..

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1768791614',resources: [
+    cacheName: 'chirpy-1768792181',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
